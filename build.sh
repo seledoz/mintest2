916f280f10c42f30064a43439eeb8f34595a232c
seledoz <donavonpfeifer@hotmail.com>
2026-09-16 08:18:59 -0600

    Include Rope Waypoint UI module in CaveBot build

--- build.sh
+++ build.sh
@@ -45,6 +45,7 @@ cat \
   src/modules/quick-controls-settings.js \
   src/ui/panel.js \
   src/modules/auto-target-v2-panel.js \
+  src/modules/cave-rope-waypoint-ui.js \
   src/main.js \
   > pz-bot.js
 
